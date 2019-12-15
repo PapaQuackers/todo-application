fa7386652c67cb6cb72a589e6e324a23573cda23
papaquackers <PapaQuackers@gmail.com>
2019-12-15 11:17:08 -0500

    Initial implementation of more reuseable menu using angular CDK for practice, next commit will refactor into new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiServicesModule } from './api-services/api-services.module';
 import { NavComponent } from './app-components/nav/nav.component';
+import {OverlayModule} from '@angular/cdk/overlay';
+import { MenuComponent } from './app-components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ApiServicesModule
+    ApiServicesModule,
+    OverlayModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [MenuComponent]
 })
 export class AppModule { }
